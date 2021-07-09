318569bb944d77eaf8bd3be6986d21388f4a723a
-l <togasakitogatoga@gmail.com>
2021-07-10 01:35:10 +0900

    Update submit.sh

--- tools/submit.sh
+++ tools/submit.sh
@@ -2,5 +2,8 @@
 
 # Usage
 # $ ./submit.sh 1 my-solution.json
+user_name=`git config user.name`
+
+curl -X POST -H "Content-Type: application/json" --data @$2 http://34.146.27.136:1323/api/problems/$1/solutions/$user_name
 
 curl -H "Authorization: Bearer $GTF_API_KEY" --data @$2 "https://poses.live/api/problems/$1/solutions"
