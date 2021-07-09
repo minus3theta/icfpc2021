0e694ae53442ec41d99260e4cb7af6f7308e2190
Raito <raito@soundhound.com>
2021-07-10 01:59:24 +0900

    [GUI] Check intersect

--- gui-tool/src/scenes/mainScene.ts
+++ gui-tool/src/scenes/mainScene.ts
@@ -25,13 +25,37 @@ class Vertice {
 
 class Edge {
   public v;
+  public graphics;
+
+  constructor(v1: Vertice, v2: Vertice) {
+    this.v = [v1, v2];
+  }
+
+  draw(): void {
+    this.graphics.clear();
+    const line = new Phaser.Geom.Line(this.v[0].x * displayRate,
+                                      this.v[0].y * displayRate,
+                                      this.v[1].x * displayRate,
+                                      this.v[1].y * displayRate);
+    this.graphics.strokeLineShape(line);
+  }
+}
+
+class HoleEdge extends Edge {
+  constructor(v1: Vertice, v2: Vertice, scene: Phaser.Scene) {
+    super(v1, v2);
+    this.graphics = scene.add.graphics({ lineStyle: { width: 2, color: 0x000000 }});
+    this.draw();
+  }
+}
+
+class FigureEdge extends Edge {
   public baseLength;
   public minLength;
   public maxLength;
-  public graphics;
 
   constructor(v1: Vertice, v2: Vertice, epsilon: number, scene: Phaser.Scene) {
-    this.v = [v1, v2];
+    super(v1, v2);
     this.baseLength = this.calcLength();
     this.graphics = scene.add.graphics({ lineStyle: { width: 4, color: 0xff0000 } })
 
@@ -42,15 +66,6 @@ class Edge {
     const dx = this.v[0].x - this.v[1].x;
     const dy = this.v[0].y - this.v[1].y;
     return dx*dx + dy*dy;
-  }
-
-  draw(): void {
-    this.graphics.clear();
-    const line = new Phaser.Geom.Line(this.v[0].x * displayRate,
-                                      this.v[0].y * displayRate,
-                                      this.v[1].x * displayRate,
-                                      this.v[1].y * displayRate);
-    this.graphics.strokeLineShape(line);
   }
 }
 
@@ -74,27 +89,35 @@ export class MainScene extends Phaser.Scene {
 
   private vertices;
   private edges;
+  private holeVertices;
   private holeEdges;
 
   private dragging = false;
   private draggingVertice;
+  private processing = false;
 
   create(): void {
     this.drawLattice();
     this.drawHole();
     this.initVerticesAndEdges();
     this.drawFigure();
+    this.checkHoleIntersect();
 
     const that = this;
     this.input.on('pointermove', function(pointer) {
       if (that.dragging) {
-        const roundX = Math.round(pointer.x / displayRate);
-        const roundY = Math.round(pointer.y / displayRate);
-        const v = that.draggingVertice;
-        v.x = roundX;
-        v.y = roundY;
-        v.resetCircle();
-        that.drawFigure();
+        if (!that.processing) {
+          that.processing = true;
+          const roundX = Math.round(pointer.x / displayRate);
+          const roundY = Math.round(pointer.y / displayRate);
+          const v = that.draggingVertice;
+          v.x = roundX;
+          v.y = roundY;
+          that.checkHoleIntersect();
+          v.resetCircle();
+          that.drawFigure();
+          that.processing = false;
+        }
       } else {
         for (const v of that.vertices) {
           if (v.circle.contains(pointer.x, pointer.y)) {
@@ -138,10 +161,10 @@ export class MainScene extends Phaser.Scene {
 
     this.edges = [];
     for (let i = 0; i < origEdges.length; i++) {
-      this.edges.push(new Edge(this.vertices[origEdges[i][0]],
-        this.vertices[origEdges[i][1]],
-        this.problemInfo.epsilon,
-        this));
+      this.edges.push(new FigureEdge(this.vertices[origEdges[i][0]],
+                                     this.vertices[origEdges[i][1]],
+                                     this.problemInfo.epsilon,
+                                     this));
     }
   }
 
@@ -156,20 +179,56 @@ export class MainScene extends Phaser.Scene {
   }
 
   drawHole(): void {
-    const holeGraphics = this.add.graphics({ lineStyle: { width: 2, color: 0x000000 }});
-    const holes = this.problemInfo.hole;
-    for (let i = 0; i < holes.length; i++) {
-      const p1 = holes[i];
-      const p2 = holes[(i+1) % holes.length];
-      const line = new Phaser.Geom.Line(p1[0] * displayRate,
-        p1[1] * displayRate,
-        p2[0] * displayRate,
-        p2[1] * displayRate);
-      holeGraphics.strokeLineShape(line);
+    const hole = this.problemInfo.hole;
+
+    this.holeVertices = [];
+    for (let i = 0; i < hole.length; i++) {
+      this.holeVertices.push(new Vertice(hole[i][0], hole[i][1], this));
+    }
+
+    this.holeEdges = [];
+    for (let i = 0; i < hole.length; i++) {
+      this.holeEdges.push(new HoleEdge(this.holeVertices[i],
+                                       this.holeVertices[(i+1) % hole.length],
+                                       this));
     }
   }
 
   drawFigure(): void {
     for (const edge of this.edges) edge.draw();
+  }
+
+  checkHoleIntersect(): void {
+    for (const holeEdge of this.holeEdges) {
+      let intersect = false;
+      for (const figureEdge of this.edges) {
+        if (this.isIntersect(holeEdge, figureEdge)) {
+          intersect = true;
+          break;
+        }
+      }
+      if (intersect) {
+        holeEdge.graphics = this.add.graphics({ lineStyle: { width: 2, color: 0x999999 } })
+      } else {
+        holeEdge.graphics = this.add.graphics({ lineStyle: { width: 2, color: 0x000000 } })
+      }
+      holeEdge.draw();
+    }
+  }
+
+  isIntersect(e1: Edge, e2: Edge): boolean {
+    const x1 = e1.v[0].x;
+    const y1 = e1.v[0].y;
+    const x2 = e1.v[1].x;
+    const y2 = e1.v[1].y;
+    const x3 = e2.v[0].x;
+    const y3 = e2.v[0].y;
+    const x4 = e2.v[1].x;
+    const y4 = e2.v[1].y;
+    const ta = (x3-x4)*(y1-y3)+(y3-y4)*(x3-x1)
+    const tb = (x3-x4)*(y2-y3)+(y3-y4)*(x3-x2)
+    const tc = (x1-x2)*(y3-y1)+(y1-y2)*(x1-x3);
+    const td = (x1-x2)*(y4-y1)+(y1-y2)*(x1-x4);
+    return ta * tb < 0 && tc * td < 0;
   }
 }
