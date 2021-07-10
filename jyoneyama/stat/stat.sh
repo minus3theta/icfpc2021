e0838c9b0ca7840db24ab9f77658cf1dfead62b8
minus3theta <yone_j_synthesis@yahoo.co.jp>
2021-07-10 22:55:44 +0900

    Support bonuses in stats

--- jyoneyama/stat/stat.sh
+++ jyoneyama/stat/stat.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -e
 
-seq 78 | xargs printf "%d.json\n" | xargs -n1 jq -r '[(.hole|length),(.figure.edges|length),(.figure.vertices|length),.epsilon]|@csv' > stat.csv
+echo 'id,holes,edges,vertices,epsilon,bonus_bonus,bonus_problem,bonus_position' > stat.csv
+paste -d , <(seq 88) \
+  <(seq 88 | xargs printf "../../problems/%d.json\n" | xargs -n1 \
+  jq -r '[(.hole|length),(.figure.edges|length),(.figure.vertices|length),.epsilon,.bonuses[0].bonus,.bonuses[0].problem,.bonuses[0].position|tostring]|@csv')\
+  >> stat.csv
