713dfbc99cd0284ea62425edc60f0970232ad0a8
minus3theta <yone_j_synthesis@yahoo.co.jp>
2021-07-11 02:47:53 +0900

    Update table definition

--- togatoga/kennel/postgres/initdb.d/01_init.sql
+++ togatoga/kennel/postgres/initdb.d/01_init.sql
@@ -4,13 +4,22 @@ CREATE TABLE solutions (
     problem_id INT NOT NULL,
     user_name TEXT,
     solution JSON NOT NULL,
-    created_at  TIMESTAMP with time zone NOT NULL
+    use_globalist BOOLEAN NOT NULL,
+    use_break_a_leg BOOLEAN NOT NULL,
+    created_at TIMESTAMP with time zone NOT NULL
 );
--- INSERT INTO submissions(problem_id, user_name, user_email, submission, created_at)
--- VALUES(
---         1,
---         'hitoshi',
---         'togatoga',
---         '{"vertices": [[20, 0],[0, 20],[40, 20],[20, 40]]}',
---         current_timestamp
---     );
+
+DROP TABLE IF EXISTS problems;
+CREATE TABLE problems (
+    id INT NOT NULL PRIMARY KEY,
+    problem JSON NOT NULL
+);
+
+CREATE TYPE bonus_type AS ENUM('GLOBALIST', 'BREAK_A_LEG');
+DROP TABLE IF EXISTS bonuses;
+CREATE TABLE bonuses (
+    id serial NOT NULL PRIMARY KEY,
+    source INT NOT NULL,
+    destination INT NOT NULL,
+    bonus bonus_type NOT NULL
+);
