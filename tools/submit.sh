6f28f5d7f38d99b629c2489a490d4cff8395eac6
Hitoshi Togasaki <togasakitogatoga@gmail.com>
2021-07-10 14:12:21 +0900

    Fix tool

--- tools/submit.sh
+++ tools/submit.sh
@@ -5,5 +5,5 @@
 
 curl -H "Authorization: Bearer $GTF_API_KEY" --data @$2 "https://poses.live/api/problems/$1/solutions"
 
-user_name=`git config user.name`
+user_name=`git config user.name | sed 's/ //g'`
 curl -X POST -H "Content-Type: application/json" --data @$2 http://34.146.27.136/api/problems/$1/solutions/$user_name
