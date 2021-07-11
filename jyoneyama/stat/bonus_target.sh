993b18e292f72c1efbdea1b8e3ae0f266e53597e
minus3theta <yone_j_synthesis@yahoo.co.jp>
2021-07-11 21:48:04 +0900

    Update stats

--- jyoneyama/stat/bonus_target.sh
+++ jyoneyama/stat/bonus_target.sh
@@ -2,12 +2,9 @@
 
 echo 'digraph G {' > bonus.dot
 
-paste -d "" <(seq 106) \
-  <(seq 106 | xargs printf "../../problems/%d.json\n" | xargs -n1 \
+paste -d "" <(seq 132) \
+  <(seq 132 | xargs printf "../../problems/%d.json\n" | xargs -n1 \
   jq -r '"->"+([.bonuses[]|.problem|tostring]|join(","))+";"')\
   >> bonus.dot
 
 echo '}' >> bonus.dot
-
-# problems with no bonuses are not handled correctly
-# manually commenting out "104->;" and "105->;"
--- jyoneyama/stat/stat.sh
+++ jyoneyama/stat/stat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-echo 'id,holes,edges,vertices,epsilon,bonus_0_bonus,bonus_0_problem,bonus_1_bonus,bonus_1_problem' > stat.csv
-paste -d , <(seq 106) \
-  <(seq 106 | xargs printf "../../problems/%d.json\n" | xargs -n1 \
-  jq -r '[(.hole|length),(.figure.edges|length),(.figure.vertices|length),.epsilon,.bonuses[0].bonus,.bonuses[0].problem,.bonuses[1].bonus,.bonuses[1].problem]|@csv')\
+echo 'id,holes,edges,vertices,epsilon,bonus_0_bonus,bonus_0_problem,bonus_1_bonus,bonus_1_problem,bonus_2_bonus,bonus_2_problem' > stat.csv
+paste -d , <(seq 132) \
+  <(seq 132 | xargs printf "../../problems/%d.json\n" | xargs -n1 \
+  jq -r '[(.hole|length),(.figure.edges|length),(.figure.vertices|length),.epsilon]+([.bonuses[]|[.bonus,.problem]]|flatten)|@csv')\
   >> stat.csv
